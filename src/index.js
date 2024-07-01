ce377e100348add4f9ba78d8358ce8f92c2c74ce
lejonie <165010214+lejonie@users.noreply.github.com>
2024-07-01 12:51:33 +0200

    added api connections for wind + humidity data

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
 }
 
 function searchCity(city) {
   let apiKey = "3bco0055182cd577e01a3fa410t5ac83";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
+  console.log(apiUrl);
 }
 
 function handleSearchSubmit(event) {
